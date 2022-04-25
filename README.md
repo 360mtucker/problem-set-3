0c41cd934da76a8dcba591ec2e7f58f871011512
360mtucker <46805953+360mtucker@users.noreply.github.com>
2022-04-25 11:33:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# module-4-problem-set-3
+Module 4 problem-set-3
---
+++ problem1.py
@@ -0,0 +1,22 @@
+"""
+NTEC 365
+YOUR NAME
+Date: 
+Name: Module 4 - Problem Set 3
+Description: Problem 1
+"""
+
+""" 
+IPO
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+Input(s): 
+Process(es): 
+Output(s): 
+"""
+
+
+def main():
+    pass  # remove this line after you write your code
+
+
+main()
